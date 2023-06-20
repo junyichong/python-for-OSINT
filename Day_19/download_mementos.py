99f34ec5caf1a505e125c772cd9d177f9925a06d
junyichong <junyichong@gmail.com>
2023-06-20 11:18:59 +0900

    Done

--- Day_19/download_mementos.py
+++ Day_19/download_mementos.py
@@ -3,11 +3,16 @@ from datetime import datetime
 
 client = wayback.WaybackClient()
 
+# num = 1
+
 for record in client.search('http://nasa.gov', to_date=datetime(1999, 1, 1)):
 
     memento = client.get_memento(record)
 
-    fileName=memento.memento_url.replace("/","-")+".html"
+    fileName=memento.memento_url.replace("/","-").replace(":","-")+".html"
+
+    # fileName= str(num) +".html"
+    # num = num + 1
 
     memento_file = open(fileName, "a")
 
